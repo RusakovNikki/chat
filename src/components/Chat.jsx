192a55514199bccb14b226e31fb31aeec1bbf2fc
RusakovNikki <rusakov.nikki@yandex.ru>
2022-11-11 02:31:54 +0300

    редизайн

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import { getAuth } from "firebase/auth"
 import { useCollectionData } from "react-firebase-hooks/firestore"
@@ -14,6 +14,7 @@ import {
 } from "firebase/firestore"
 
 const Chat = () => {
+  const scrollToLastMessage = useRef(null)
   const db = getFirestore()
   const auth = getAuth()
   const [user] = useAuthState(auth)
@@ -21,6 +22,11 @@ const Chat = () => {
   const [messages] = useCollectionData(
     query(collection(db, "messages"), orderBy("createdAt"))
   )
+
+  useEffect(() => {
+    console.log(user)
+    scrollToLastMessage?.current?.scrollIntoView()
+  }, [messages])
 
   const sendMessage = async () => {
     await addDoc(collection(db, "messages"), {
@@ -35,39 +41,77 @@ const Chat = () => {
 
   return (
     <Container>
+      <div className="onlineUsersBlock">
+        {/* {user &&
+          user?.map((item) => (
+            <div className="onlineUsersBlock__user">
+              <div
+                style={{
+                  margin: "10px",
+                  width: "fit-content",
+                  padding: 15,
+                }}
+              >
+                <Grid container>
+                  <Avatar src={item.photoURL} />
+                  <div>{item.displayName}</div>
+                </Grid>
+                <div>{item.text}</div>
+              </div>
+            </div>
+          ))} */}
+      </div>
       <Grid
         container
         justifyContent={"center"}
         style={{ height: window.innerHeight - 50 }}
       >
         <div
+          className="container"
           style={{
             width: "80%",
-            height: "70vh",
-            border: "1px solid black",
+            height: "80vh",
+            boxShadow: "0 0 31px #ccc",
             overflow: "auto",
+            marginTop: "20px",
           }}
         >
           {messages &&
             messages.map((message) => (
-              <div
-                style={{
-                  margin: "10px",
-                  border:
-                    user.uid === message.uid
-                      ? "2px solid green"
-                      : "2px solid blue",
-                  marginLeft: user.uid === message.uid ? "auto" : "10px",
-                  width: "fit-content",
-                  padding: 15,
-                }}
-              >
-                <Grid container>
-                  <Avatar src={message.photoURL} />
-                  <div>{message.displayName}</div>
-                </Grid>
-                <div>{message.text}</div>
-              </div>
+              <>
+                <div
+                  className="user"
+                  style={{
+                    margin: "10px",
+                    marginLeft: user.uid === message.uid ? "auto" : "10px",
+                    width: "fit-content",
+                    padding: 15,
+                  }}
+                >
+                  <div
+                    className="user__item"
+                    style={{
+                      flexDirection:
+                        user.uid === message.uid ? "row-reverse" : "row",
+                    }}
+                  >
+                    <Avatar src={message.photoURL} className="user__avatar" />
+                    <div
+                      className="user__text"
+                      style={{
+                        background:
+                          user.uid === message.uid
+                            ? "rgb(34, 134, 255)"
+                            : "rgb(140 91 255)",
+                      }}
+                    >
+                      {/* <div>{message.displayName}</div> */}
+                      <div style={{ fontSize: "16px" }}>{message.text}</div>
+                    </div>
+                  </div>
+                </div>
+                <div ref={scrollToLastMessage}></div>
+              </>
             ))}
         </div>
         <Grid
@@ -77,16 +121,22 @@ const Chat = () => {
           style={{ width: "80%" }}
         >
           <form
-            style={{ width: "100%" }}
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              height: "30px",
+              alignItems: "baseline",
+              marginBottom: "20px",
+            }}
             onSubmit={(e) => {
               e.preventDefault()
               sendMessage()
             }}
           >
-            <TextField
-              fullWidth
-              maxRows={2}
-              variant={"outlined"}
+            <input
+              type="text"
+              className="form__input"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
             />
